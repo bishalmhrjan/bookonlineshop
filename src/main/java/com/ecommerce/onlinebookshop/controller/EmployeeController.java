e53641a4a43a898d886092abf2f70756884651b1
bishal1993 <maharjanbishal19@gmail.com>
2025-02-12 15:15:11 +0100

    Employee controller has been done with Response Entity

--- src/main/java/com/ecommerce/onlinebookshop/controller/EmployeeController.java
+++ src/main/java/com/ecommerce/onlinebookshop/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.ecommerce.onlinebookshop.controller;
 import com.ecommerce.onlinebookshop.model.entity.Employee;
 import com.ecommerce.onlinebookshop.service.EmployeeService;
 import com.ecommerce.onlinebookshop.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,31 +14,36 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    private final EmployeeService EmployeeService;
+    private final EmployeeService employeeService;
 
 
 
     public EmployeeController(EmployeeService cartItemItemService) {
-        this.EmployeeService = cartItemItemService;
+        this.employeeService = cartItemItemService;
     }
 
     @GetMapping
-    public List<Employee> getAllCartItems(){
-        return EmployeeService.getAllEmployees();
+    public ResponseEntity<List<Employee>> getAllCartItems(){
+        List<Employee> employees= employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
     }
 
     @GetMapping("/{id}")
-    public Optional<Employee> getCartItemById(@PathVariable Long id){
-        return EmployeeService.getEmployeeById(id);
+    public ResponseEntity<Employee> getCartItemById(@PathVariable Long id){
+        Optional<Employee> employee= employeeService.getEmployeeById(id);
+      return   employee.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound()
+        .build());
     }
 
     @PostMapping
-    public Employee createCartItem(@RequestBody Employee Employee){
-        return EmployeeService.addEmployee(Employee);
+    public ResponseEntity<Employee> createCartItem(@RequestBody Employee employee){
+        Employee employeeToSave = employeeService.addEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeToSave);
     }
 
     @DeleteMapping
-    public void deleteCartItem(@PathVariable Long id){
-        EmployeeService.deleteEmployee(id);
+    public ResponseEntity<Void> deleteCartItem(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
     }
 }
