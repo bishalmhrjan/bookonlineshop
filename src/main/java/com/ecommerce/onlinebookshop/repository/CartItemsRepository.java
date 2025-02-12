85de29a8eeea4897f3063e9fec937587cc9905cc
bishal1993 <maharjanbishal19@gmail.com>
2025-02-12 09:49:28 +0100

    Cartitem is done!!

--- src/main/java/com/ecommerce/onlinebookshop/repository/CartItemsRepository.java
+++ src/main/java/com/ecommerce/onlinebookshop/repository/CartItemsRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemsRepository extends JpaRepository<CartItem,Long> {
+
 }
+
--- src/main/java/com/ecommerce/onlinebookshop/service/CartItemService.java
+++ src/main/java/com/ecommerce/onlinebookshop/service/CartItemService.java
@@ -1,7 +1,33 @@
 package com.ecommerce.onlinebookshop.service;
 
+import com.ecommerce.onlinebookshop.model.entity.CartItem;
+import com.ecommerce.onlinebookshop.repository.CartItemsRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartItemService {
+    private final CartItemsRepository cartItemrepository;
+
+    public CartItemService(CartItemsRepository cartItemrepository) {
+        this.cartItemrepository = cartItemrepository;
+    }
+
+    public List<CartItem> getCartItems(Long id){
+        return cartItemrepository.findAll();
+    }
+
+    public Optional<CartItem> getCartitemByCartItemId(Long id){
+        return cartItemrepository.findById(id);
+    }
+
+    public CartItem addCartItem(CartItem cartItem){
+        return cartItemrepository.save(cartItem);
+    }
+
+    public void deleteCartItem(Long id){
+        cartItemrepository.deleteById(id);
+    }
 }
