9e5d1179bcebb58c65d9f57ee93f267138ae5fcb
bishal1993 <maharjanbishal19@gmail.com>
2025-02-12 13:35:51 +0100

    Admin controller class is implemented with Responseentity

--- src/main/java/com/ecommerce/onlinebookshop/controller/AdminController.java
+++ src/main/java/com/ecommerce/onlinebookshop/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.ecommerce.onlinebookshop.controller;
 
 import com.ecommerce.onlinebookshop.model.entity.admin.Admin;
 import com.ecommerce.onlinebookshop.service.AdminService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,23 +20,27 @@ public class AdminController {
     }
 
     @GetMapping
-    public List<Admin> getAllAdmin(){
-        return adminService.getAllAdmin();
+    public ResponseEntity<List<Admin>> getAllAdmin(){
+        List<Admin> admins= adminService.getAllAdmin();
+        return ResponseEntity.ok(admins);
     }
 
     @GetMapping("/{id}")
-    public Optional<Admin> getAdminById(@PathVariable Long id){
-        return adminService.getAdminById(id);
+    public ResponseEntity<Admin> getAdminById(@PathVariable Long id){
+        Optional<Admin> admin= adminService.getAdminById(id);
+        return admin.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
     }
 
 
     @PostMapping
-    public Admin createAdmin(@RequestBody Admin admin){
-        return adminService.addAmin(admin);
+    public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin){
+         Admin savedAdmin= adminService.addAmin(admin);
+         return ResponseEntity.status(HttpStatus.CREATED).body(savedAdmin);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAdmin(@PathVariable Long id){
+    public ResponseEntity<Void> deleteAdmin(@PathVariable Long id){
         adminService.deleteAdmin(id);
+        return ResponseEntity.noContent().build();
     }
 }
