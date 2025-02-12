2db9f8f97cf1437098c0d1b9f7b89114a783b741
bishal1993 <maharjanbishal19@gmail.com>
2025-02-12 10:20:39 +0100

    customer service is finish

--- src/main/java/com/ecommerce/onlinebookshop/service/CustomerService.java
+++ src/main/java/com/ecommerce/onlinebookshop/service/CustomerService.java
@@ -1,7 +1,33 @@
 package com.ecommerce.onlinebookshop.service;
 
+import com.ecommerce.onlinebookshop.model.entity.Customer;
+import com.ecommerce.onlinebookshop.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id){
+        return customerRepository.findById(id);
+    }
+
+    public Customer addCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long id){
+        customerRepository.deleteById(id);
+    }
 }
