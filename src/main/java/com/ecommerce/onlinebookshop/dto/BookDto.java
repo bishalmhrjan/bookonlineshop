86c594694f19d19f1d650e9d033a093cd023c782
bishal1993 <maharjanbishal19@gmail.com>
2025-02-13 19:42:45 +0100

    bookdto and book mapper class is finish

--- src/main/java/com/ecommerce/onlinebookshop/dto/BookDto.java
+++ src/main/java/com/ecommerce/onlinebookshop/dto/BookDto.java
@@ -1,4 +1,35 @@
 package com.ecommerce.onlinebookshop.dto;
 
-public class BookDto {
+import com.ecommerce.onlinebookshop.model.entity.book.BookDescription;
+import com.ecommerce.onlinebookshop.model.entity.book.BookMetaData;
+import com.ecommerce.onlinebookshop.model.enums.BookType;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public  class BookDto {
+
+    private Long id;
+    private String title;
+    private double price;
+    private int stockQuantity;
+
+
+    private BookType category;
+
+
+    private BookMetaData bookMetaData;
+
+
+    private BookDescription description;
 }
--- src/main/java/com/ecommerce/onlinebookshop/mapper/BookMapper.java
+++ src/main/java/com/ecommerce/onlinebookshop/mapper/BookMapper.java
@@ -1,4 +1,35 @@
 package com.ecommerce.onlinebookshop.mapper;
 
+import com.ecommerce.onlinebookshop.dto.BookDto;
+import com.ecommerce.onlinebookshop.model.entity.book.Book;
+
 public class BookMapper {
+    public static BookDto mapToBookDto(Book book){
+        BookDto bookDto = BookDto.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .price(book.getPrice())
+                .stockQuantity(book.getStockQuantity())
+                .category(book.getCategory())
+                .bookMetaData(book.getBookMetaData())
+                .description(book.getDescription())
+                .build();
+
+        return  bookDto;
+    }
+
+    public static Book mapToBook(BookDto bookDto){
+        Book book = Book.builder()
+                .id(bookDto.getId())
+                .title(bookDto.getTitle())
+                .price(bookDto.getPrice())
+                .stockQuantity(bookDto.getStockQuantity())
+                .category(bookDto.getCategory())
+                .bookMetaData(bookDto.getBookMetaData())
+                .description(bookDto.getDescription())
+                .build();
+
+        return book;
+
+    }
 }
