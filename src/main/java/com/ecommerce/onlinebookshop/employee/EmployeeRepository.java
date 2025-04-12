9616d3206cf746f6874670d96825a5b46ba4d5e6
bishal1993 <maharjanbishal19@gmail.com>
2025-04-12 08:37:20 +0200

    Employee Repo is coded

--- src/main/java/com/ecommerce/onlinebookshop/employee/EmployeeRepository.java
+++ src/main/java/com/ecommerce/onlinebookshop/employee/EmployeeRepository.java
@@ -1,9 +1,32 @@
 package com.ecommerce.onlinebookshop.employee;
 
 import com.ecommerce.onlinebookshop.model.entity.Employee;
+import com.ecommerce.onlinebookshop.model.entity.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
  public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+/**
+ * jpql for first,lastname
+ */
+@Query("SELECT e FROM Employee e WHERE e.email = :email")
+List<Employee> findEmployeeByEmail(@Param("email") String email);
+
+ @Query("SELECT  e FROM Employee e WHERE e.firstName = :firstName")
+ List<Employee> findEmployeeByFirstName(@Param("firstName") String firstName);
+
+ @Query("SELECT e FROM Employee e WHERE e.lastName= :lastName")
+ List<Employee> findEmployeeByLastName(@Param("lastName") String lastName);
+
+
+
+ @Query("SELECT  e FROM Employee e WHERE e.shippingAddress.cityName = :city")
+ List<Employee> findEmployeeByCity(@Param("city") String city);
+
+
 }
