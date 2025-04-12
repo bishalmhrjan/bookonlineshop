6a555d4b2bdd52a4d40e23ec10ffc1ba9bc4533a
bishal1993 <maharjanbishal19@gmail.com>
2025-04-12 08:38:14 +0200

    Admin Repo is coded

--- src/main/java/com/ecommerce/onlinebookshop/admin/AdminRepository.java
+++ src/main/java/com/ecommerce/onlinebookshop/admin/AdminRepository.java
@@ -3,6 +3,8 @@ package com.ecommerce.onlinebookshop.admin;
 import com.ecommerce.onlinebookshop.model.entity.Customer;
 import com.ecommerce.onlinebookshop.model.entity.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,8 +13,24 @@ import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends  JpaRepository<Admin,Long>{
-// findbyEmail
-    Optional<Admin> findByEmail(String email);
+
+    @Query("SELECT a FROM Admin a WHERE a.email = :email")
+    List<Admin> findAdminByEmail(@Param("email") String email);
+
+    @Query("SELECT  a FROM Admin a WHERE a.firstName = :firstName")
+    List<Admin> findAdminByFirstName(@Param("firstName") String firstName);
+
+    @Query("SELECT a FROM Admin a WHERE a.lastName= :lastName")
+    List<Admin> findAdminByLastName(@Param("lastName") String lastName);
+
+
+
+    @Query("SELECT  a FROM Admin a WHERE  a.shippingAddress.cityName = :city")
+    List<Admin> findAdminByCity(@Param("city") String city);
+
+
+
+
 
 
 
