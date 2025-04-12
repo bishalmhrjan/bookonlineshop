f012bbd44632689671a388d40aa02735cc801178
bishal1993 <maharjanbishal19@gmail.com>
2025-04-12 08:37:59 +0200

    Cart Repo is coded

--- src/main/java/com/ecommerce/onlinebookshop/cart/CartRepository.java
+++ src/main/java/com/ecommerce/onlinebookshop/cart/CartRepository.java
@@ -1,9 +1,27 @@
 package com.ecommerce.onlinebookshop.cart;
 
 import com.ecommerce.onlinebookshop.model.entity.Cart;
+import com.ecommerce.onlinebookshop.model.entity.Order;
+import com.ecommerce.onlinebookshop.model.entity.book.Book;
+import com.ecommerce.onlinebookshop.model.enums.BookType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    @Query("SELECT c FROM Cart c WHERE c.totalPrice <= :totalPrice")
+    List<Cart> findCartLesserThantotalPrice(@Param("totalPrice") double totalPrice);
+
+
+    @Query("SELECT  c FROM Cart c WHERE c.customer.firstName = :firstName")
+    List<Cart> findCartByCustomerFirstName(@Param("firstName")  String firstName);
+
+
 }
